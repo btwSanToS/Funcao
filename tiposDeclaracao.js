54b2b2cbcecb3c38adb779349c82da01f6d5b4d2
btwSanToS <andrelmsantos.87@gmail.com>
2024-03-27 14:44:18 -0300

    declaração simples de funções, 3 casos.

---
+++ tiposDeclaracao.js
@@ -0,0 +1,16 @@
+// Function declaration
+function soma(x, y) {
+    return x + u
+}
+// Função declarada.
+
+// Function expression
+const sub = function (x, y) {
+    return x - y
+}
+// Função Anônima.
+
+// Named function expression
+const mult = function mult(x, y) {
+    return x * y
+}
